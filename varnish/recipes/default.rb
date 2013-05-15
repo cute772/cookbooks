817a0a904ec9f1f6077e9fd5f8140a9dc2d80092
jccrosby <john@realeyes.com>
2013-05-15 12:40:52 -0600

    Init

---
+++ varnish/recipes/default.rb
@@ -0,0 +1,77 @@
+# Cookbook Name:: varnish
+# Recipe:: default
+# Author:: Joe Williams <joe@joetify.com>
+# Contributor:: Patrick Connolly <patrick@myplanetdigital.com>
+#
+# Copyright 2008-2009, Joe Williams
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if platform?("ubuntu", "debian")
+  include_recipe "apt"
+  apt_repository "varnish-cache.org" do
+    uri "http://repo.varnish-cache.org/#{:platform}/"
+    distribution node['lsb']['codename']
+    components ["main"]
+    key "http://repo.varnish-cache.org/debian/GPG-key.txt"
+    deb_src true
+    notifies :run, resources(:execute => "apt-get update"), :immediately
+  end
+end
+
+pkgs = value_for_platform(
+  [ "centos", "redhat", "fedora" ] => {
+    "default" => %w{ varnish-release varnish }
+  },
+  [ "debian", "ubuntu" ] => {
+    "default" => %w{ varnish }
+  }
+)
+
+pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# package "varnish"
+
+template "#{node['varnish']['dir']}/#{node['varnish']['vcl_conf']}" do
+  source node['varnish']['vcl_source']
+  if node['varnish']['vcl_cookbook']
+    cookbook node['varnish']['vcl_cookbook']
+  end
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, "service[varnish]"
+end
+
+template node['varnish']['default'] do
+  source "custom-default.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, "service[varnish]"
+end
+
+service "varnish" do
+  supports :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+service "varnishlog" do
+  supports :restart => true, :reload => true
+  action [ :enable, :start ]
+end
